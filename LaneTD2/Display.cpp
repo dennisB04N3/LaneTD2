4fea4aeb84821ed221d101adaa9bbf0141be7c68
dennisB04N3 <bohne212@gmail.com>
2022-01-11 19:38:22 +0100

    update

--- LaneTD2/Display.cpp
+++ LaneTD2/Display.cpp
@@ -1,1 +1,32 @@
 #include "Display.h"
+
+Display::Display()
+{
+	this->init();
+	viewSpeed = 2000.f;
+}
+
+
+void Display::init()
+{
+	//for later: configuration file?(menu point: options) ->google
+	window.create(sf::VideoMode(1920, 1080), "Lane TD");
+	view.setSize(1920.f, 1080.f);
+	view.setCenter(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
+	window.setView(view);
+}
+
+void Display::moveView(char key, float deltaTime)
+{
+	switch (key)
+	{
+	case 'W':
+		this->view.move(0.f, -viewSpeed * deltaTime);
+		break;
+	case 'S':
+		this->view.move(0.f, viewSpeed * deltaTime);
+		break;
+	default:
+		break;
+	}
+}
--- LaneTD2/Game.cpp
+++ LaneTD2/Game.cpp
@@ -1,1 +1,131 @@
 #include "Game.h"
+Game::Game()
+{
+	this->gridSize = 50;
+	this->rows = 20;
+	this->columns = 20;
+
+
+	if (!this->font.loadFromFile("D:/workspaces/libs/SFML-2.5.1/examples/island/resources/sansation.ttf"))
+	{
+		std::cout << "Font not found" << std::endl;
+	}
+	else
+	{
+		this->text_MP.setFont(font);
+		this->text_MP.setCharacterSize(12);
+		this->text_MP.setFillColor(sf::Color::White);
+		this->text_MP.setPosition(20.f, 20.f);
+	}
+
+	//do all before display, give display necessary items for drawing
+	this->display = new Display();
+	this->window = this->display->getWindow();
+}
+
+Game::~Game()
+{
+	delete this->display;
+	//delete this->world;
+	this->display = nullptr;
+	//this->world = nullptr;
+	this->window = nullptr;
+}
+
+void Game::start()
+{
+	float deltaTime = 0;
+
+	while (window->isOpen())
+	{
+		deltaTime = clock.restart().asSeconds();
+		updateMP();
+		sf::Event event;
+
+		while (window->pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window->close();
+			}
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				/*if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					this->world->changeTile(&MPGrid);
+				}*/
+			}
+			if (event.type == sf::Event::KeyPressed)
+			{
+				/*if (event.key.code == sf::Keyboard::W)
+				{
+					this->display->moveView('W', deltaTime);
+
+				}
+				if (event.key.code == sf::Keyboard::S)
+				{
+					this->display->moveView('S', deltaTime);
+
+				}*/
+			}
+		}
+		/*if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			std::cout << "another method" << std::endl;
+			this->display->moveView('S', deltaTime);
+		}*/
+		update();
+		window->clear();
+
+		//this->world->draw(*this->window);
+
+		window->setView(window->getDefaultView());
+		window->draw(text_MP);
+		window->display();
+	}
+}
+
+void Game::updateMP()
+{
+	MPWindow = sf::Mouse::getPosition(*this->window);
+	window->setView(this->window->getView());
+	MPView = window->mapPixelToCoords(MPWindow);
+
+	if (MPView.x > 0)
+	{
+		MPGrid.x = static_cast<unsigned>((MPView.x) / gridSize);
+	}
+	if (MPView.y > 0)
+	{
+		MPGrid.y = static_cast<unsigned>((MPView.y) / gridSize);
+	}
+}
+
+void Game::update()
+{
+	std::string str;
+
+	str = "Window:\t" + std::to_string(MPWindow.x) + " " + std::to_string(MPWindow.y) + "\n"
+		+ "View:\t" + std::to_string(MPView.x) + " " + std::to_string(MPView.y) + "\n"
+		+ "Grid:\t" + std::to_string(MPGrid.x) + " " + std::to_string(MPGrid.y);
+
+	text_MP.setString(str);
+}
+
+
+/*For later use:
+
+sf::Vertex vertices[] =
+{
+	sf::Vertex(sf::Vector2f(100, 0), sf::Color::Red),
+	sf::Vertex(sf::Vector2f(100, 100), sf::Color::Red)
+};
+// draw it
+
+sf::VertexArray lines(sf::LinesStrip, 4);
+lines[0].position = sf::Vector2f(100, 100);
+lines[1].position = sf::Vector2f(200, 100);
+lines[2].position = sf::Vector2f(300, 500);
+lines[3].position = sf::Vector2f(400, 200);
+window->draw(vertices, 2, sf::Lines);
+window->draw(lines);*/
